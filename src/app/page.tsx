e0817b0998628ef641fee04b35638f4d50e47a35
Duy Nguyen <duyngha@gmail.com>
2023-09-24 21:52:30 +0700

    feat: dominance chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
-import Image from 'next/image'
 import { Source_Code_Pro } from 'next/font/google'
-import styles from './page.module.css'
+import DomiChart from './domi-chart'
 
-const scp = Source_Code_Pro({ subsets: ['latin'] })
+const font = Source_Code_Pro({ subsets: ['latin'] })
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('http://localhost:8082/dominances')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const data = await getData()
+  
   return (
-    <main className={scp.className}>
-      <h1 className="text-3xl font-bold underline">Hello, I am Cinance App!</h1>
+    <main className={font.className}>
+      <div className="container max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold underline text-center">Hello, I am Cinance App!</h1>
+        <DomiChart data={data}/>
+      </div>
     </main>
   )
 }
